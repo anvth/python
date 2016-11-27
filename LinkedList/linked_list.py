6458ded20d19c3270305d6a49f5e87ecaf51ae74
Anvith Shivakumara <s.anvith@gmail.com>
2016-11-28 00:44:39 +0530

    Unordered Linked List Base Implementation

---
+++ LinkedList/linked_list.py
@@ -0,0 +1,54 @@
+from node import Node
+
+class UnorderedList(object):
+	def __init__(self):
+		self.head = None
+
+	def is_empty(self):
+		return self.head == None
+
+	def display(self):
+		if self.head is None:
+			print "Nothing to display!!"
+			return
+
+		current = self.head
+		while current != None:
+			print current.get_data()
+			current = current.get_next()
+		print "--------------------------------"
+
+	def add(self, item):
+		temp = Node(item)
+		temp.set_next(self.head)
+		self.head = temp
+
+	def add_to_end(self, item):
+		current = self.head
+		while current.get_next() != None:
+			current = current.get_next()
+		current.set_next(Node(item))
+
+	def delete(self):
+		self.head = self.head.get_next()
+
+	def delete_at_end(self):
+		current = self.head
+		while current.get_next().get_next() != None:
+			current = current.get_next()
+		current.set_next(None)
+
+
+if __name__ == '__main__':
+	obj = UnorderedList()	
+	obj.add(2)
+	obj.display()
+	obj.add(1)
+	obj.display()
+	obj.add_to_end(3)
+	obj.display()
+	obj.delete()
+	obj.display()
+	obj.delete_at_end()
+	obj.display()
+			
